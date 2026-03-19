0d5e599dca03cc539e20ed11a9b03d449ab23f60
MRgIue <joe@loebachconsulting.com>
2026-03-19 15:13:46 -0500

    Scaffold Next.js project with TypeScript and Tailwind

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function RootPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-semibold text-gray-600">Page not found</h1>
+    </div>
+  );
+}
